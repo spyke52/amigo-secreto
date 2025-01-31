c9ee176c5bf2eee1e9009d1ea22f88104803136b
spyke52 <129613159+spyke52@users.noreply.github.com>
2025-01-31 06:08:32 -0300

    declaracion de variables y funcion

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+// lista de nombres de los amigos
+let listaAmigos = [];
+let sorteoRealizado = false; // variable para saber si ya se realizo el sorteo
+
+// funcion para validar si es texto.
+function esTextoValido(valor) {
+    // exprecion para validar texto y espacio
+    const regex = /^[A-Za-zÁÉÍÓÚáéíóúñÑ\s]+$/;
+    return regex.test(valor);
+}
